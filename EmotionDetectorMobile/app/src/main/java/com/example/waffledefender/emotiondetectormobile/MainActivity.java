725b5d0ad796351f9f5708c5a6cfff1d93c509a7
Mason Campbell <WaffleDefend29@gmail.com>
2017-10-23 15:15:03 -0600

    More Progress on Settings

--- EmotionDetectorMobile/app/src/main/java/com/example/waffledefender/emotiondetectormobile/MainActivity.java
+++ EmotionDetectorMobile/app/src/main/java/com/example/waffledefender/emotiondetectormobile/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.waffledefender.emotiondetectormobile;
 
+import android.content.Intent;
 import android.drm.DrmStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_selection, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
